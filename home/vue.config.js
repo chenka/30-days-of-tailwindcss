580eb3bab05a8005e1b61799026af395d2e14d67
Sarunyhot Suvannachoti <sarunyhot@omise.co>
2020-06-08 16:49:11 +0700

    Update piblicPath

--- home/vue.config.js
+++ home/vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/home/dist/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/30-days-of-tailwindcss/home/dist/"
+      : "/",
 }
