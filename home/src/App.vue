16513788f5677dbd68d70cd51c8d0cca74da84a3
Sarunyhot Suvannachoti <sarunyhot@omise.co>
2020-06-08 16:55:16 +0700

    Fixed image path

--- home/src/App.vue
+++ home/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-<body class="antialiased " style="font-family: 'Roboto', sans-serif;">
+<body class="antialiased" style="font-family: 'Roboto', sans-serif;">
   <div class="px-10 py-20">
     <div class="mb-10 text-center">
       <div class="text-4xl font-black font">30 Days of Tailwind CSS</div>
       <div class="text-lg">My project made in 30 days</div>
     </div>
     <div
-      class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10 sm:gap-12  sm:w-full w-408 mx-auto"
+      class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10 sm:gap-12 sm:w-full w-408 mx-auto"
     >
-    <div v-for="(card,index) in cards" :key="card.title">
+      <div v-for="(card,index) in cards" :key="card.title">
         <Card
           :img="card.img"
           :imgAlt="card.title"
@@ -33,17 +33,17 @@ export default {
     return {
       cards: [
         {
-          img:"/images/day-1.png",
-          title:"Statistics",
-          href: "/ui/day-1/public/index.html"
+          img: "images/day-1.png",
+          title: "Statistics",
+          href: "ui/day-1/public/index.html"
         },
         {
-          img:"/images/day-4.png",
-          title:"File Manager - Cloud Storage",
-          href: "/ui/day-2/public/index.html"
+          img: "images/day-4.png",
+          title: "File Manager - Cloud Storage",
+          href: "ui/day-2/public/index.html"
         }
       ]
-    }
+    };
   }
 };
 </script>
